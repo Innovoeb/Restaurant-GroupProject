eaa5a2f24d607267a37b9b9f52b2a125948f59e9
Stephanie Revis <stephanie6304@gmail.com>
2019-03-29 14:50:39 -0700

    dunno

---
+++ whipper.js
@@ -0,0 +1,11 @@
+// $.get('https://obscure-tundra-54269.herokuapp.com/bar-food').done(function(data){
+//     console.log(data)
+//     data.appetizers.map(function(item){
+//         console.log(item)
+//     })
+// })
+
+
+$.get('https://www.google.com/maps/place/PunchCode/@36.1583864,-115.1546902,17z/data=!3m1!4b1!4m5!3m4!1s0x80c8c39ab242ee05:0x14135188ae6c3322!8m2!3d36.1583864!4d-115.1525015').done(function(data){
+    console.log(data)
+})
