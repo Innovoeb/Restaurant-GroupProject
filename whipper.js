929b7a5361ac4c85677b4d4ad0c5233884289bec
Innovoeb <Innovoeb@gmail.com>
2019-03-29 13:15:52 -0700

    completed menu javascript

--- whipper.js
+++ whipper.js
@@ -15,4 +15,121 @@ $(document).ready(function(){
         }
     })
 
+    let appetizersStr = barFood.appetizers.map(a => {
+      var description;
+      if (a.description.length > 40) {
+        description = a.description.substring(0, 40) + '...'
+      }else {
+        description = a.description
+      }
+
+      var spicy = '';
+      var glutenfree = '';
+      var vegetarian = '';
+
+      if (a.extra.spicy === true) {
+        spicy = `<p class="spicy"><i class="fas fa-pepper-hot"></i></p>`
+      }
+      if (a.extra.glutenfree === true) {
+        glutenfree = `<div class="glutenfree"><p><i class="fas fa-ban"></i>Gluten</p></div>`
+      }
+      if (a.extra.vegetarian === true) {
+        vegetarian = `<p class="vegetarian"><i class="fas fa-leaf"></i></p>`
+      }
+
+    return `
+    <div class="menu-items">
+      <img src ="http://placehold.it/200/200">
+      <p class="dish-name">${a.name}</p>
+      <p class="description"><abbr title="${a.description}">${description}</abbr></p>
+      <p class="price">$${a.price}</p>
+      <div class="extra">
+        ${spicy}
+        ${glutenfree}
+        ${vegetarian}
+      </div>
+    </div>
+      `
+    }).join("")
+
+    $('#appetize').html(appetizersStr)
+
+    let entreesStr = barFood.entrees.map(a => {
+      var description;
+      if (a.description.length > 40) {
+        description = a.description.substring(0, 40) + '...'
+      }else {
+        description = a.description
+      }
+
+      var spicy = '';
+      var glutenfree = '';
+      var vegetarian = '';
+
+      if (a.extra.spicy === true) {
+        spicy = `<p class="spicy"><i class="fas fa-pepper-hot"></i></p>`
+      }
+      if (a.extra.glutenfree === true) {
+        glutenfree = `<div class="glutenfree"><p><i class="fas fa-ban"></i>Gluten</p></div>`
+      }
+      if (a.extra.vegetarian === true) {
+        vegetarian = `<p class="vegetarian"><i class="fas fa-leaf"></i></p>`
+      }
+
+    return `
+    <div class="menu-items">
+      <img src ="http://placehold.it/200/200">
+      <p class="dish-name">${a.name}</p>
+      <p class="description"><abbr title="${a.description}">${description}</abbr></p>
+      <p class="price">$${a.price}</p>
+      <div class="extra">
+        ${spicy}
+        ${glutenfree}
+        ${vegetarian}
+      </div>
+    </div>
+      `
+    }).join("")
+
+    $('#entrees').html(entreesStr)
+
+    let dessertsStr = barFood.desserts.map(a => {
+      var description;
+      if (a.description.length > 40) {
+        description = a.description.substring(0, 40) + '...'
+      }else {
+        description = a.description
+      }
+
+      var spicy = '';
+      var glutenfree = '';
+      var vegetarian = '';
+
+      if (a.extra.spicy === true) {
+        spicy = `<p class="spicy"><i class="fas fa-pepper-hot"></i></p>`
+      }
+      if (a.extra.glutenfree === true) {
+        glutenfree = `<div class="glutenfree"><p><i class="fas fa-ban"></i>Gluten</p></div>`
+      }
+      if (a.extra.vegetarian === true) {
+        vegetarian = `<p class="vegetarian"><i class="fas fa-leaf"></i></p>`
+      }
+
+    return `
+    <div class="menu-items">
+      <img src ="http://placehold.it/200/200">
+      <p class="dish-name">${a.name}</p>
+      <p class="description"><abbr title="${a.description}">${description}</abbr></p>
+      <p class="price">$${a.price}</p>
+      <div class="extra">
+        ${spicy}
+        ${glutenfree}
+        ${vegetarian}
+      </div>
+    </div>
+      `
+    }).join("")
+
+    $('#desserts').html(dessertsStr)
+
 })
